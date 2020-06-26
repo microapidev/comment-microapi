37b9b5b924b5177039b6127bbadc273e24198e7d
Dave <callremz@gmail.com>
2020-06-26 18:06:12 +0100

    ft: add success responseHandler

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use("/comments", commentRoutes);
 app.use(["/", "/documentation"], documentationRoutes);
 
 // Invalid route error handler
+<<<<<<< HEAD
 app.use("*", (req, res, next) => {
   const error = new CustomError(
     404,
@@ -53,6 +54,23 @@ app.use("*", (req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
   errorHandler(err, req, res, next);
+=======
+app.use("*", (req, res) => {
+  res.status(404).send({
+    message: `Oops. The route ${req.method} ${req.originalUrl} is not recognised.`,
+  });
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.sendStatus(err.status || 500);
+  res.send("error");
+>>>>>>> ft: add success responseHandler
 });
 
 module.exports = app;
