fa0c2efd656c918759359b6edc3eb985e2f74d8c
EricoMartin <martinirex@yahoo.co.uk>
2020-06-27 08:57:37 +0100

    upvote and downvote feature

--- controller/commentsController.js
+++ controller/commentsController.js
@@ -10,27 +10,31 @@ exports.voteComment = async (req, res) => {
     const _id = req.params.commentId;
     const { voteType, email } = req.body;
     await Comments.findById({ _id }).then(async (comments) => {
-      let numOfUpVotes = 0;
-      let numOfDownVotes = 0;
-      if (voteType === "upvote") numOfUpVotes += 1;
-      if (voteType === "downvote") numOfDownVotes += 1;
-      const total = numOfUpVotes + numOfDownVotes;
-      const data = {
-        commentId: _id,
-        total_votes: total,
-        upvotes: numOfUpVotes,
-        downvotes: numOfDownVotes,
-      };
-      comments.voteType = voteType;
-      comments.totalVotes = total;
-      const user = await User.findOne({ email });
-      if (voteType === "upvote") comments.upVotes.push(user);
-      if (voteType === "downvote") comments.downVotes.push(user);
-      res.json({
-        message: "Comment Voted Successfully!",
-        response: "Ok",
-        data: data,
-      });
+      try {
+        let numOfUpVotes = 0;
+        let numOfDownVotes = 0;
+        if (voteType === "upvote") numOfUpVotes += 1;
+        if (voteType === "downvote") numOfDownVotes += 1;
+        const total = numOfUpVotes + numOfDownVotes;
+        const data = {
+          commentId: _id,
+          total_votes: total,
+          upvotes: numOfUpVotes,
+          downvotes: numOfDownVotes,
+        };
+        comments.voteType = voteType;
+        comments.totalVotes = total;
+        const user = await User.findOne({ email });
+        if (voteType === "upvote") comments.upVotes.push(user);
+        if (voteType === "downvote") comments.downVotes.push(user);
+        res.json({
+          message: "Comment Voted Successfully!",
+          response: "Ok",
+          data: data,
+        });
+      } catch (err) {
+        errHandler(err, res);
+      }
     });
   } catch (err) {
     errHandler(err, res);
