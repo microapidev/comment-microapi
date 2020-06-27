261c9d1433b47cccc49d45a776bafa382cf51135
Dave <callremz@gmail.com>
2020-06-27 04:08:44 +0100

    feature: add middleware

---
+++ middleware/validation.js
@@ -0,0 +1,24 @@
+import CustomError from "../utils/customError";
+
+const validationMiddleware = (requestSchema) => {
+  return (req, res, next) => {
+    const validations = ["headers", "params", "query", "body"].map((key) => {
+      const schema = requestSchema[key];
+      const value = req[key];
+      const validate = () =>
+        schema ? schema.validate(value) : Promise.resolve({});
+      return validate().then((result) => ({ [key]: result }));
+    });
+    return Promise.all(validations)
+      .then(() => {
+        next();
+      })
+      .catch((validationError) => {
+        const message = validationError.details.map((d) => d.message).join("");
+        const err = new CustomError(400, message);
+        next(err);
+      });
+  };
+};
+
+export default validationMiddleware;
