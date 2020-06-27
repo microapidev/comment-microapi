29c1f2662eae9896d5a3841634eaf8a1b0270bd7
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-06-27 09:21:40 +0100

    Added test cases for GET Requests

--- __test__/comments.test.js
+++ __test__/comments.test.js
@@ -1,24 +1,35 @@
 const app = require('../server');
+const Comments = require('../models/comments');
 const supertest = require('supertest');
 const request = supertest(app);
 const mongoose = require('mongoose');
 
-describe('Comments Endpoints', () => {
+describe('GET Comments Endpoints', () => {
   it('gets all comments from the db', async (done) => {
     const res = await request.get('/comments');
     expect(res.status).toBe(200);
     expect(res.body.status).toBe('success');
-      expect(res.body).toHaveProperty('data');
+    expect(res.body.message).toBe('Comments Retrieved Successfully');
+    done();
     done();
   });
 
-  it('posts a new comment to the db', async (done) => {
-    const res = await request
-      .post('/comments')
-      .set('Accept', 'application/json')
-      .send(req.body);
+  it('gets all comment for a particular ref', async (done) => {
+    const refId = request.params.refId;
+    const res = await request.get('/comments/refs/' + refId);
+    expect(request.body).toHaveProperty('refId');
     expect(res.status).toBe(200);
     expect(res.body.status).toBe('success');
+    expect(res.body.data.refId).toBe('refId');
+    done();
+  });
+
+  it('gets all replies for a comment', async (done) => {
+    const commentId = request.params.commentId;
+    const res = await request.get('/comments/replies/' + commentId);
+    expect(res.status).toBe(200);
+    expect(res.body.status).toBe('success');
+    expect(res.body.data.commentId).toBe(commentId);
     done();
   });
 
