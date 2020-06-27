0c47b0564a8ab0a509a5f4cbc114239f91d42a6c
Ekeyekwu Oscar <45172025+oscar-ekeyekwu@users.noreply.github.com>
2020-06-27 14:41:09 +0100

    Revert "[FEATURE REQUEST] controller function upvote or downote a user comment #55"

--- models/comments.js
+++ models/comments.js
@@ -14,9 +14,7 @@ const CommentSchema = new Schema(
       type: Number,
       default: 0,
     },
-
     replies: [{ type: Schema.Types.ObjectId, ref: "Replies" }],
-
     upVotes: [
       {
         type: Schema.Types.ObjectId,
