4d5d70c9adcf0a11907f536a2b157f1c84f0c3cc
piouson <piouson@gmail.com>
2020-06-27 16:38:38 +0100

    Fix lint errors

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json"],
   testRegex: [".spec.js$", ".test.js$"],
   coverageDirectory: "./coverage",
-  testEnvironment: "node",
+  //testEnvironment: "node",
   globalSetup: "./__test__/config/globalSetup.js",
   globalTeardown: "./__test__/config/globalTeardown.js",
+  preset: "@shelf/jest-mongodb",
 };
