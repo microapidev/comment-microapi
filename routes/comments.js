fe3fb669f77752b613ee5f36696d3c5bc90eafb8
neymarjimoh <jemohkunle2007@gmail.com>
2020-06-27 10:11:53 +0100

    feat: added the functionality for users to fag off comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,8 @@
 const repliesRoutes = require("./replies");
 const router = require("express").Router();
+const commentController = require("../controller/commentsController");
 
 router.use("/comments/replies", repliesRoutes);
 
+router.patch("/:commentId/flag", commentController.flagComment);
 module.exports = router;
