b9a5a0d2f5fd4f4a50296261f98c6e6de454b656
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-28 11:14:06 +0100

    users can delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const commentController = require("../controller/commentsController");
 router.use("/:commentId/replies", repliesRoutes);
 
 //using the commentId as a query paramerter in the route
-router.delete("/:userId", commentController.deleteComment)
+router.delete("comment/:userId", commentController.deleteComment);
 
 router.patch("/:commentId/flag", commentController.flagComment);
 module.exports = router;
