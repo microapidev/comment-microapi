92c7f7fdd11ebe7c93b76ca5a0da4dce39a1065c
Lihemen <lihemen@gmail.com>
2020-06-29 19:37:02 +0100

    [FEATURE]: Controller to update comment #11

--- controller/commentsController.js
+++ controller/commentsController.js
@@ -3,9 +3,10 @@
 // const Replies = require("../models/replies");
 const Comments = require("../models/comments");
 const mongoose = require("mongoose");
-// const CustomError = require("../utils/customError");
+const CustomError = require("../utils/customError");
 // const User = require("../models/users");
 const errHandler = require("../utils/errorhandler");
+const responseHandler = require("../utils/responseHandler");
 
 exports.flagComment = async (req, res) => {
   try {
@@ -50,4 +51,38 @@ exports.flagComment = async (req, res) => {
   } catch (error) {
     errHandler(error, res);
   }
+};
+
+exports.updateComment = async (req, res, next) => {
+  const comment_id = req.params.commentId;
+  const commentBody = req.body.commentBody;
+  if (!commentBody || commentBody.trim() == "") {
+    return res.status(400).json({ message: "Invalid comment body" });
+  }
+  Comments.findById(comment_id)
+    .exec()
+    .then((comment) => {
+      if (!comment) {
+        return next(new CustomError(404, "Comment not found!!"));
+      } else {
+        Comments.updateOne(
+          { _id: comment_id },
+          { $set: { commentBody: commentBody, isEdited: true } }
+        )
+          .then(() => {
+            return responseHandler(
+              res,
+              201,
+              commentBody,
+              "Updated successfully"
+            );
+          })
+          .catch((err) => {
+            return next(new CustomError(400, "Update failed, try again", err));
+          });
+      }
+    })
+    .catch((err) => {
+      return next(new CustomError(500, "Something went wrong, try again", err));
+    });
 };
--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,9 @@
 const repliesRoutes = require("./replies");
 const router = require("express").Router();
 const commentController = require("../controller/commentsController");
+const { updateComment } = require("../controller/commentsController");
 
 router.use("/:commentId/replies", repliesRoutes);
-
+router.patch("/:commentId", updateComment);
 router.patch("/:commentId/flag", commentController.flagComment);
 module.exports = router;
