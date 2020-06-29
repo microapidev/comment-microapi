97c9f81c32f5aa70a7fdf3336ed5bbb58f182407
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-29 18:24:42 +0200

    feat: add validation rules for POST /comments

--- utils/validationRules/index.js
+++ utils/validationRules/index.js
@@ -1,8 +1,10 @@
 const getAllCommentsSchema = require("./comments/getAllCommentsSchema");
+const createCommentSchema = require("./comments/createCommentSchema");
 
 /**
  * Object containing schema validations for the endpoints.
  */
 module.exports = {
   getAllCommentsSchema,
+  createCommentSchema,
 };
