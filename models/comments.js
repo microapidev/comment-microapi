15b613bcf9a687d7991d6722b67e687b7fd74ce3
Dave <callremz@gmail.com>
2020-06-29 21:39:54 +0100

    feature: propsed Comment model

--- models/comments.js
+++ models/comments.js
@@ -3,31 +3,44 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema(
   {
-    refId: Number,
-    commentBody: {
+    refId: String, // field used by applications to point to reference comment location in their app
+    applicationId: {
+      //the id of the application that this comment belongs to
+      type: Schema.Types.ObjectId,
+      ref: "Applications",
+    },
+    ownerId: {
+      // field used for emails, userId from the applcation used to identify its users
       type: String,
       required: true,
     },
-    commentOrigin: String,
-    isFlagged: { type: Boolean, default: false },
-    numOfFlags: {
-      type: Number,
-      default: 0,
+    commentBody: {
+      // field contains comment body
+      type: String,
+      required: true,
     },
-    replies: [{ type: Schema.Types.ObjectId, ref: "Replies" }],
-    upVotes: [
+    commentOrigin: String, // field is free for use by applications as seen fit
+    replies: [{ type: Schema.Types.ObjectId, ref: "Replies" }], // array of replies
+    flags: [
+      // contains array of ownerId allowing only one flag per user
       {
-        type: Schema.Types.ObjectId,
-        ref: "Users",
+        type: String, // array of ownerId
+      },
+    ],
+    upVotes: [
+      // contains array of ownerId allowing only one vote per user either up or neither
+      // push ownerId onto array to vote, remove from array to unvote
+      {
+        type: String, // array of ownerId
       },
     ],
     downVotes: [
+      // contains array of ownerId allowing only one vote per user down or neither
+      // push ownerId onto array to vote, remove from array to unvote
       {
-        type: Schema.Types.ObjectId,
-        ref: "Users",
+        type: String, // array of ownerId
       },
     ],
-    commentOwner: { type: Schema.Types.ObjectId, ref: "Users" },
   },
   { timestamps: true }
 );
