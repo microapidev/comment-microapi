9252c813a41333ccd25f0e8f40931d42314c675e
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-06-29 20:54:28 +0100

    fixed lint errors

--- __test__/api/comment-patch.test.js
+++ __test__/api/comment-patch.test.js
@@ -7,30 +7,31 @@ const request = supertest(app);
 const { describeIfEndpoint } = require("../helpers/conditionalTests");
 
 describeIfEndpoint(
-    "PATCH",
-    "/comments/:commentId",
-    "PATCH /comments/:commentId",
-    () => {
-      it("Updates a comment", async () => {
-        const comment = new CommentModel({
-          commentBody: "this is a comment",
-          commentOwnerName: "userName",
-          commentOwnerEmail: "useremail@email.com",
-          commentOrigin: "123123",
-          commentOwner: mongoose.Types.ObjectId(),
-        });
-        await comment.save();
-  
-        const commentId = comment._id;
-  
-        const res = await request.patch(`/comments/${commentId}`).send({commentOwnerEmail: comment.commentOwnerEmail,commentBody:"New Comment Update"});
-  
-        
-        expect(res.status).toBe(200);
-        expect(res.body.status).toBe("success");
-        expect(res.body.data.commentBody).toBeTruthy();      
-        expect(res.body.data.commentOwnerEmail).toBeTruthy();
+  "PATCH",
+  "/comments/:commentId",
+  "PATCH /comments/:commentId",
+  () => {
+    it("Updates a comment", async () => {
+      const comment = new CommentModel({
+        commentBody: "this is a comment",
+        commentOwnerName: "userName",
+        commentOwnerEmail: "useremail@email.com",
+        commentOrigin: "123123",
+        commentOwner: mongoose.Types.ObjectId(),
       });
-    }
-  );
-  
+      await comment.save();
+
+      const commentId = comment._id;
+
+      const res = await request.patch(`/comments/${commentId}`).send({
+        commentOwnerEmail: comment.commentOwnerEmail,
+        commentBody: "New Comment Update",
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.body.status).toBe("success");
+      expect(res.body.data.commentBody).toBeTruthy();
+      expect(res.body.data.commentOwnerEmail).toBeTruthy();
+    });
+  }
+);
