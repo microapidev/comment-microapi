b3f0ad29de88ebbf2032a2482e26738e6f398536
EricoMartin <47243793+EricoMartin@users.noreply.github.com>
2020-06-30 19:49:18 +0100

    downvote comment endpoint (#172)

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,11 @@ const router = require("express").Router();
 const commentController = require("../controller/commentsController");
 const { appAuthMW } = require("../middleware/auth");
 
+// upvote a comment
 router.patch("/:commentId/votes/upvote", commentController.upvoteComment);
+
+// downvote comment
+router.patch("/:commentId/votes/downvote", commentController.downvoteComment);
 router.use(appAuthMW);
 router.use("/:commentId/replies", repliesRoutes);
 
