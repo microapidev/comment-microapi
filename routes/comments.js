8c0752fb63925823ccbdb2d0a263b67a086bca6b
Dave <callremz@gmail.com>
2020-06-30 13:00:23 +0100

    feat: add dummy auth

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,9 @@
 const repliesRoutes = require("./replies");
 const router = require("express").Router();
 const commentController = require("../controller/commentsController");
+const { appAuthMW } = require("../middleware/auth");
 
+router.use(appAuthMW);
 router.use("/:commentId/replies", repliesRoutes);
 
 router.patch("/:commentId/flag", commentController.flagComment);
