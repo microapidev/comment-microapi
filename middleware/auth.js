1c67b15e84c3c37b2590c470b385995f55e0dd49
Dave <callremz@gmail.com>
2020-07-01 07:12:02 +0100

    refactor: modify requestProperty to token

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,10 @@ const jwtMW = require("express-jwt");
 
 exports.appAuthMW = jwtMW({
   secret: process.env.APP_SECRET,
+  requestProperty: "token",
 });
 
 exports.orgAuthMW = jwtMW({
   secret: process.env.ORG_SECRET,
+  requestProperty: "token",
 });
