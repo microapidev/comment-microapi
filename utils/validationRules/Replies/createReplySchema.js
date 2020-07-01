ee619984652c2eba6372f840760fa13d6584aafb
papa28x4 <swissfair@yahoo.co.uk>
2020-07-01 10:15:40 +0100

    createReplySchema

---
+++ utils/validationRules/Replies/createReplySchema.js
@@ -0,0 +1,25 @@
+const Joi = require("@hapi/joi");
+
+/**
+ * Schema validation for POST '/comments/{commentId}/replies'
+ */
+const createReplySchema = {
+  options: {
+    allowUnknown: true,
+  },
+
+  header: Joi.object({
+    authorization: Joi.string().required(),
+  }),
+
+  params: Joi.object({
+    commentId: Joi.string().length(24).required(),
+  }),
+
+  body: Joi.object().keys({
+    ownerId: Joi.string().required(),
+    content: Joi.string().required(),
+  }),
+};
+
+module.exports = createReplySchema;
