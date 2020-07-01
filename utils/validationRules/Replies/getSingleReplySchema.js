2674544f49601e202d78616d8f95fe6f7673b2b5
papa28x4 <swissfair@yahoo.co.uk>
2020-07-01 12:49:33 +0100

    single reply schema

---
+++ utils/validationRules/Replies/getSingleReplySchema.js
@@ -0,0 +1,22 @@
+const Joi = require("@hapi/joi");
+
+/**
+ * Schema validation for GET '/comments/{commentId}/replies/{replyId}'
+ */
+
+const getSingleReplySchema = {
+  options: {
+    allowUnknown: true,
+  },
+
+  headers: Joi.object({
+    authorization: Joi.string().required(),
+  }),
+
+  params: Joi.object().keys({
+    commentId: Joi.string().length(24).required(),
+    replyId: Joi.string().length(24).required(),
+  }),
+};
+
+module.exports = getSingleReplySchema;
