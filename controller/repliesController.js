92b05959e02e4f04670ddf151702ba362e92cb7a
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-01 19:41:05 +0200

    feat: add update reply controller method

--- controller/repliesController.js
+++ controller/repliesController.js
@@ -135,6 +135,46 @@ const createReply = async (req, res, next) => {
   }
 };
 
+// Updates a reply
+exports.updateReply = async (req, res, next) => {
+  const { commentId, replyId } = req.params;
+  const { content, ownerId } = req.body;
+
+  try {
+    let comment = await Comments.findById(commentId);
+
+    if (!comment) {
+      return next(new CustomError(404, "Comment not found or deleted"));
+    }
+
+    let reply = await Replies.findById(replyId);
+
+    if (!reply) {
+      return next(new CustomError(404, "Reply not found or deleted"));
+    }
+
+    await reply.updateOne(
+      { _id: replyId },
+      { $set: { content: content, isEdited: true } }
+    );
+
+    return responseHandler(
+      res,
+      200,
+      { content: content, ownerId: ownerId },
+      "Updated sucessfully"
+    );
+  } catch (error) {
+    return next(
+      new CustomError(
+        500,
+        "Something went wrong, please try again later.",
+        error
+      )
+    );
+  }
+};
+
 //PATCH downvote a reply
 const downvoteReply = async (req, res, next) => {
   const commentId = req.params.commentId;
