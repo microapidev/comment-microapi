2774a62854f585fbaa0117a63cd62648dd607994
Dave <callremz@gmail.com>
2020-07-01 07:48:43 +0100

    refactor: fix credentialsRequired field

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,11 @@ const jwtMW = require("express-jwt");
 exports.appAuthMW = jwtMW({
   secret: process.env.APP_SECRET,
   requestProperty: "token",
-  credentialsRequired: process.env.DISABLE_AUTH,
+  credentialsRequired: !process.env.DISABLE_AUTH,
 });
 
 exports.orgAuthMW = jwtMW({
   secret: process.env.ORG_SECRET,
   requestProperty: "token",
-  credentialsRequired: process.env.DISABLE_AUTH,
+  credentialsRequired: !process.env.DISABLE_AUTH,
 });
