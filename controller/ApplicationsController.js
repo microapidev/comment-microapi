c3380116f57b5d166acf6cf274b90ad3c7c641e8
Alao-Abiodun <abiodunDev@gmail.com>
2020-07-02 23:46:06 +0100

    feat/GET-all-application-of-an-organization

---
+++ controller/ApplicationsController.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const CustomError = require("../utils/customError");
+const responseHandler = require("../utils/responseHandler");
+// Application model
+const Applications = require("../models/applications");
+const Organizations = require("../models/organizations");
+
+exports.getAllApplicationOrg = async (req, res, next) => {
+  try {
+    const { orgId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(orgId)) {
+      return next(new CustomError(400, "Invalid ID"));
+    }
+
+    const organization = await Organizations.find({ _id: orgId });
+    if (!organization) {
+      return next(new CustomError(400, "organizations not found"));
+    }
+
+    const applicationId = await Applications.findById({});
+    if (!applicationId) {
+      return next(new CustomError(400, "Application ID not found"));
+    }
+
+    // check if application id is for the organization.
+    if (!organization.applicationId)
+      return next(
+        new CustomError(
+          401,
+          "The Application ID is not recommended for this organization"
+        )
+      );
+
+    const data = {
+      applicationId: organization.applicationId,
+      applicationName: organization.applicationName,
+    };
+    responseHandler(
+      res,
+      200,
+      data,
+      "Organization appId retrieved successfully"
+    );
+  } catch (err) {
+    return next(
+      new CustomError(500, "Something went wrong, Try again later", err)
+    );
+  }
+};
