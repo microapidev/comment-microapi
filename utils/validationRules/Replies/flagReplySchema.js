6b86fc4347b37e984e3835f697b1bdc6b8cd938e
reichx <66280338+reichx@users.noreply.github.com>
2020-07-02 17:35:53 +0100

    Create validation schema to flag a comment/reply

---
+++ utils/validationRules/Replies/flagReplySchema.js
@@ -0,0 +1,25 @@
+const Joi = require("@hapi/joi");
+
+/**
+ * Schema validation for PATCH '/comments/{commentId}/replies/{replyId}/flag'
+ */
+const flagReplySchema = {
+  options: {
+    allowUnknown: true,
+  },
+
+  header: Joi.object({
+    authorization: Joi.string().required(),
+  }),
+
+  params: Joi.object().keys({
+    commentId: Joi.string().required(),
+    replyId: Joi.string().required(),
+  }),
+
+  body: Joi.object().keys({
+    ownerId: Joi.string().required(),
+  }),
+};
+
+module.exports = flagReplySchema;
