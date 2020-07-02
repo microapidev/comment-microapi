cbce567f33b4b74bcb5138523768a3bcd8e4499a
charles <charlesclinton2003@gmail.com>
2020-07-02 21:01:39 +0100

    had pulled to pull the new update due

--- controller/commentsController.js
+++ controller/commentsController.js
@@ -404,3 +404,40 @@ exports.getCommentVotes = async (req, res, next) => {
     );
   }
 };
+exports.getSingleComment = async (req, res, next) => {
+  const { refId } = req.query;
+  const commentId = req.params.commentId;
+  const applicationId = req.headers.token; //this will be retrieved from decoded api token after full auth implementation
+  const query = { applicationId: applicationId, _id: commentId };
+  if (refId) query.refId = refId;
+  try {
+    await Comments.find(query)
+      .then((comments) => {
+        const comment = comments.map((comment) => {
+          return {
+            commentId: comment._id,
+            refId: comment.refId,
+            applicationId: comment.applicationId,
+            ownerId: comments.ownerId,
+            content: comment.content,
+            origin: comment.origin,
+            numOfVotes: comment.upVotes.length + comment.downVotes.length,
+            numOfUpVotes: comment.upVotes.length,
+            numOfDownVotes: comment.downVotes.length,
+            numOfFlags: comment.flags.length,
+            numOfReplies: comment.replies.length,
+            // createdAt: comment.createdAt,
+            // updatedAt: comment.updatedAt,
+          };
+        });
+        responseHandler(res, 200, comment, `Comment Retrieved Successfully`);
+      })
+      .catch((err) => {
+        return next(
+          new CustomError(500, "Something went wrong, please try again", err)
+        );
+      });
+  } catch (err) {
+    return next(new CustomError(401, `Something went wrong ${err}`));
+  }
+};
