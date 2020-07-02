ea0e1e3be8728b7710c55fac1670347810770736
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-02 14:30:07 +0200

    refactor: create index file to export all individual methods as one object

---
+++ controller/commentsController/index.js
@@ -0,0 +1,28 @@
+// CREATE
+const createSingleComment = require("./createSingleComment");
+
+// DELETE
+const deleteSingleComment = require("./deleteSingleComment");
+
+// GET
+const getAllComments = require("./getAllComments");
+const getSingleComment = require("./getSingleComment");
+const getSingleCommentVotes = require("./getSingleCommentVotes");
+
+// PATCH
+const updateSingleComment = require("./updateSingleComment");
+const updateSingleCommentUpVotes = require("./updateSingleCommentUpVotes");
+const updateSingleCommentDownVotes = require("./updateSingleCommentDownVotes");
+const updateSingleCommentFlags = require("./updateSingleCommentFlags");
+
+module.exports = {
+  createSingleComment,
+  deleteSingleComment,
+  getAllComments,
+  getSingleComment,
+  getSingleCommentVotes,
+  updateSingleComment,
+  updateSingleCommentUpVotes,
+  updateSingleCommentDownVotes,
+  updateSingleCommentFlags,
+};
