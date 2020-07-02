4eec8b763186ca46f722098b07657bc999473c28
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-02 15:05:12 +0200

    fix: remove unused variables

--- controller/commentsController/getSingleComment.js
+++ controller/commentsController/getSingleComment.js
@@ -1,4 +1,13 @@
-const getSingleComment = async (req, res, next) => {
+/**
+ * @author
+ *
+ * Gets a single comment
+ *
+ * @param {*} req - The request object
+ * @param {*} res - The response object
+ * @param {*} next - The function executed to call the next middleware
+ */
+const getSingleComment = async () => {
   throw new Error("Not implemented");
 };
 
