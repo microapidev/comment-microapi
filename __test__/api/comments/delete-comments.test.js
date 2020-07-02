bfdf1822b19cc10c902212f2004c51e2ff03caa2
piouson <piouson@gmail.com>
2020-07-02 12:27:36 +0100

    Refactor tests for comment deletion
    - refactor comment delete request for status 200
    - add comment delete request for status 404
    - indicate missing tests for status 400, 401 and 402
    
    See #241

--- __test__/api/comments/delete-comments.test.js
+++ __test__/api/comments/delete-comments.test.js
@@ -1,1 +1,65 @@
-/** DELETE ​/comments​/{commentId} */
+const app = require("../../server");
+const supertest = require("supertest");
+const CommentModel = require("../../models/comments");
+const mongoose = require("mongoose");
+const request = supertest(app);
+import { describeIfEndpoint } from "../helpers/conditionalTests";
+
+describeIfEndpoint(
+  "DELETE",
+  "/comments/:commentId",
+  "DELETE '/comments/:commentId'",
+  () => {
+    // test 200 success
+    test("Should delete a comment", async () => {
+      const comment = new CommentModel({
+        content: "this is a comment",
+        ownerId: "useremail@email.com",
+        applicationId: global.application._id,
+      });
+      await comment.save();
+
+      const res = await request
+        .delete(`/comments/${comment._id}`)
+        .set("Authorization", `bearer ${global.appToken}`)
+        .send({
+          ownerId: comment.ownerId,
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.body.status).toEqual("success");
+      expect(res.body.data.content).toEqual(comment.content);
+      expect(res.body.data.ownerId).toEqual(comment.ownerId);
+      expect(res.body.data.applicationId).toEqual(
+        comment.ownerId.applicationId
+      );
+    });
+
+    // test 404 not found error
+    test("Should fail to delete comment not found", async () => {
+      const comment = {
+        _id: mongoose.Types.ObjectId(),
+        content: "this is a comment",
+        ownerId: "rogueuser@email.com",
+        applicationId: global.application._id,
+      };
+
+      const res = await request
+        .delete(`/comments/${comment._id}`)
+        .set("Authorization", `bearer ${global.appToken}`)
+        .send({
+          ownerId: comment.ownerId,
+        });
+
+      expect(res.status).toBe(404);
+      expect(res.body.status).toEqual("error");
+      expect(res.body.data.length).toBe(0);
+    });
+
+    // test 400 bad request
+
+    // test 401 authentication error
+
+    // test 402 validation error
+  }
+);
