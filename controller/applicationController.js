e1ca6b8b324d4487dffd631aabb52cc940574c76
Simeon <adegbolasimeon@gmail.com>
2020-07-03 02:15:44 +0100

    fix conflict

---
+++ controller/applicationController.js
@@ -0,0 +1,22 @@
+const responseHandler = require("../utils/responseHandler");
+const { getAppToken } = require("../utils/auth/tokenGenerator");
+
+const getApplicationToken = async (req, res, next) => {
+  const { applicationId } = req.params;
+  const { adminId } = req.body;
+  try {
+    const token = await getAppToken(applicationId, adminId);
+    return responseHandler(
+      res,
+      201,
+      { applicationToken: token },
+      "successfully generated token"
+    );
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = {
+  getApplicationToken,
+};
--- routes/application.js
+++ routes/application.js
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const applicationsController = require("../controllers/applicationsController");
+const { orgAuthMW } = require("../middleware/auth");
+
+router.use(orgAuthMW);
 
 /**
  * POST routes
  */
 router.post("/", applicationsController.createSingleApplication);
+// get token for an application
+router.post("/:applicationId/token", applicationController.getApplicationToken);
 
 /**
  * GET routes
