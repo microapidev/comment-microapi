6a51e6cfbed63bd49217d9ffc82b17b3d4c1b0dd
Dave <callremz@gmail.com>
2020-07-04 06:51:25 +0100

    feat: create new admin controller

---
+++ controller/adminControllers.js
@@ -0,0 +1,78 @@
+const Admin = require("../models/admins");
+const CustomError = require("../utils/customError");
+const responseHandler = require("../utils/responseHandler");
+const mongoose = require("mongoose");
+const { hashPassword } = require("../utils/auth/passwordUtils");
+
+const adminCtrl = {
+  async createAdmin(req, res, next) {
+    //get arguments
+    const { fullName, email, password } = req.body;
+    const { adminId, organizationId } = req.token;
+
+    //check if admin account is valid and exists in organization
+    if (!mongoose.Types.ObjectId.isValid(adminId)) {
+      next(new CustomError(400, "Invalid adminId"));
+      return;
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(organizationId)) {
+      next(new CustomError(400, "Invalid organizationId"));
+      return;
+    }
+
+    const admin = await Admin.find({
+      _id: adminId,
+      organizationId: organizationId,
+    });
+    if (!admin) {
+      throw new CustomError(
+        401,
+        "You are not authorized to access this resource"
+      );
+    }
+
+    //---- TO-DO password should prolly be system generated for new acccounts -----
+    //hash password
+    let hashedPassword;
+    try {
+      hashedPassword = await hashPassword(password);
+    } catch (error) {
+      next(new CustomError(400, "A password processing error occured"));
+    }
+
+    //save new admin in organization
+    let newAdmin;
+    try {
+      newAdmin = new Admin({
+        fullname: fullName,
+        email: email,
+        password: hashedPassword,
+        organizationId: organizationId,
+      });
+
+      await newAdmin.save();
+    } catch (error) {
+      const errorType =
+        error.code === 11000 ? ": admin account already exists" : "";
+
+      next(
+        new CustomError(
+          400,
+          "An error occured creating admin account" + errorType
+        )
+      );
+      return;
+    }
+
+    //return new adminId
+    return responseHandler(
+      res,
+      201,
+      { adminId: newAdmin._id },
+      "Admin account created successfully"
+    );
+  },
+};
+
+module.exports = adminCtrl;
