376620f38b0fe7af250e49c01c329fe9bc2fb9d6
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-07-04 08:45:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 //setup app routes
-app.use("/comments", commentRoutes);
 app.use("/v1/comments", commentRoutes);
 app.use("/v1/organizations", organizationsRoutes);
 app.use(["/", "/v1", "/v1/documentation"], documentationRoutes);
+app.use("/v1/applications", applicationsRoutes);
 
 // Invalid route error handler
 app.use("*", (req, res, next) => {
