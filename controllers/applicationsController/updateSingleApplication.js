5a711bf6d9c3a600c84e0eadfb9f71d0cf8c9337
EricoMartin <martinirex@yahoo.co.uk>
2020-07-07 13:09:46 +0100

    POST /applications - update app properties

--- controllers/applicationsController/updateSingleApplication.js
+++ controllers/applicationsController/updateSingleApplication.js
@@ -13,28 +13,26 @@ const responseHandler = require("../utils/responseHandler");
  * @param {*} next - The function executed to call the next middleware
  */
 
-  //update an applications details/properties
-  const updateAppController = async (req, res, next) => {
-    const { name } = req.body;
-    const { applicationId } = req.params;
+//update an applications details/properties
+const updateAppController = async (req, res, next) => {
+  const { name } = req.body;
+  const { applicationId } = req.params;
 
-    try {
-      if (!mongoose.Types.ObjectId.isValid(applicationId)) {
-        next(new CustomError(422, "invalid ID"));
-        return;
-      }
-      const newAppDetails = await Applications.findById({ _id: applicationId });
-      if (!newAppDetails)
-        next(new CustomError(404, "Application Id not found"));
-      newAppDetails.name = name;
-      newAppDetails.save();
-      const data = { newAppDetails };
-      const message = "Application details updated!";
-      return responseHandler(res, 200, data, message);
-    } catch (e) {
-      return next(new CustomError(401, `Something went wrong ${e}`));
+  try {
+    if (!mongoose.Types.ObjectId.isValid(applicationId)) {
+      next(new CustomError(422, "invalid ID"));
+      return;
     }
-  };
-
+    const newAppDetails = await Applications.findById({ _id: applicationId });
+    if (!newAppDetails) next(new CustomError(404, "Application Id not found"));
+    newAppDetails.name = name;
+    newAppDetails.save();
+    const data = { newAppDetails };
+    const message = "Application details updated!";
+    return responseHandler(res, 200, data, message);
+  } catch (e) {
+    return next(new CustomError(401, `Something went wrong ${e}`));
+  }
+};
 
 module.exports = updateAppController;
