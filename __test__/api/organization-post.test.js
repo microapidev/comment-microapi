32161d8c10f4b65b095a402a4a6f9e329764d144
Lihemen <lihemen@gmail.com>
2020-07-07 14:31:52 +0100

    initial commit #253

---
+++ __test__/api/organization-post.test.js
@@ -0,0 +1,31 @@
+const app = require("../../server");
+const supertest = require("supertest");
+const OrganizationModel = require("../../models/organizations");
+const AdminModel = require("../../models/admins");
+const request = supertest(app);
+
+const { describeIfEndpoint } = require("../helpers/conditionalTests");
+const { response } = require('../../server');
+const { hashPassword } = require('../../utils/auth/passwordUtils');
+const { getOrgToken } = require('../../utils/auth/tokenGenerator');
+
+describeIfEndpoint("POST", "/v1/organizations", "POST /v1/organizations", () => {
+  test('should create organization and its admin', async () => {
+    const response = await request
+      .post("/v1/organizations")
+      .send({
+        organizationName: "HNG",
+        organizationEmail: "hng@email.com",
+        secret: "hdfiafifqnfnvovv",
+        adminName: "John Doe",
+        adminEmail: "johndoe@gmail.com",
+        adminPassword: "adminpassword"
+      })
+
+    expect(response.status).toBe(201);
+    expect(response.body.data.organizationId).toBeTruthy()
+    expect(response.body.data.adminId).toBeTruthy()
+    expect(response.body.data.organizationToken).toBeTruthy()
+  });
+});
+
