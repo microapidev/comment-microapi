51d60356d80180811198389c291b3af165e96e1f
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-10 11:47:07 +0200

    fix: use previous version

--- __test__/utils/commentHandler.test.js
+++ __test__/utils/commentHandler.test.js
@@ -1,20 +1,28 @@
 const commentHandler = require("../../utils/commentHandler");
-const CommentModel = require("../../models/comments");
+const { ObjectId } = require("mongoose").Types;
 
 describe("Unit test for commentHandler function", () => {
-  const mockComment = new CommentModel({
+  const mockComment = {
+    replies: [],
+    flags: [],
+    upVotes: [],
+    downVotes: [],
+    _id: ObjectId("5f0504f6f7b28607cc80714f"),
     refId: "4edd40c86762e0fb12000003",
-    applicationId: global.application._id,
+    applicationId: ObjectId("5f0504f6f7b28607cc80714a"),
     content: "A mock comment from user1",
     ownerId: "user1@email.com",
     origin: "b12000003",
-  });
+    createdAt: new Date("2020-07-07T23:27:50.431Z"),
+    updatedAt: new Date("2020-07-07T23:27:50.431Z"),
+    __v: 0,
+  };
 
   test("Should match expected object", () => {
     const expected = {
-      commentId: mockComment.id,
+      commentId: String(mockComment._id),
       refId: mockComment.refId,
-      applicationId: mockComment.applicationId,
+      applicationId: String(mockComment.applicationId),
       ownerId: mockComment.ownerId,
       content: mockComment.content,
       origin: mockComment.origin,
