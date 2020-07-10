db85100f4518205e690068c76f4726f6a089db9b
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-07-10 12:16:09 +0100

    createSingleApplication.test.js

---
+++ __test__/api/applications/createSingleApplication.test.js
@@ -0,0 +1,35 @@
+const app = require("../../../server");
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+//const applicationHandler = require("../../../utils/responseHandler");
+const { getOrgToken } = require("../../../utils/auth/tokenGenerator");
+const request = supertest(app);
+
+describe("POST /applications", () => {
+    let orgToken, url;
+    let invalidToken = "5f08err3e9dced1ad07err48b9b57a";
+
+    beforeAll(async () => {
+        orgToken = await getOrgToken(global.organization._id, global.admin._id);
+        url = "/v1/applications";
+    });
+  
+    it("Should successfully create an application", async () => {
+      let res = await request
+        .post(url)
+        .set("Authorization", `bearer ${orgToken}`)
+        .send({ name: "test_app" });
+      expect(res.status).toEqual(201);
+      expect(res.body.status).toEqual("success");
+    });
+
+    it("Should return a 401 error for invalid Token", async () => {
+        let res = await request
+          .post(url)
+          .set("Authorization", `bearer ${invalidToken}`)
+          .send({ name: "test_app" });
+  
+        expect(res.status).toEqual(401);
+        expect(res.body.status).toEqual("error");
+      });
+});
