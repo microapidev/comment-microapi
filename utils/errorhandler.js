3dba0ece56385480ea5af203e21b3eeec82beff4
Dave <callremz@gmail.com>
2020-07-13 14:09:31 +0100

    feat: include detailed error in dev and test envs

--- utils/errorhandler.js
+++ utils/errorhandler.js
@@ -15,9 +15,14 @@ const errorHandler = (err, req, res) => {
     });
     // if this is an unknown/uncaught error
   } else {
+    let serverError = "";
+    // show detailed error message in DEV or TEST environment
+    if (["dev", "test"].indexOf(process.env.NODE_ENV) > -1) {
+      serverError = `: ${err.message}`;
+    }
     res.status(500).json({
       status: "error",
-      error: "Internal server error",
+      error: `Internal server error${serverError}`,
       data: [], // no data to return
     });
     //we want to see the actual error
