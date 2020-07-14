83b9817a1ec9df1860a63443706cbceda54f9ff8
Dave <callremz@gmail.com>
2020-07-14 13:58:11 +0100

    feat: create default sysadmin function

---
+++ utils/auth/msadmin.js
@@ -0,0 +1,64 @@
+const MsAdmin = require("../../models/msadmins");
+const { hashPassword } = require("./passwordUtils");
+
+//create default super-admin if not exists
+exports.createDefaultAdmin = async () => {
+  //search for default admin user
+  try {
+    const msAdmin = await MsAdmin.findOne({
+      sysdefined: true,
+      role: "superadmin",
+    });
+
+    if (msAdmin) {
+      console.log("\n \t Minimal account found");
+      return msAdmin.id;
+    }
+  } catch (error) {
+    throw new Error(`Error getting minimal account: ${error.message}`);
+  }
+
+  try {
+    //if not found read in SUPER_ADMIN_PASSWORD, SUPER_ADMIN_EMAIL from env
+    console.log("\n \t Minimal account not found ... creating");
+
+    //check email
+    const superAdminEmail = process.env.SUPER_ADMIN_EMAIL;
+
+    //if no password found exit requesting the variable
+    if (!superAdminEmail) {
+      throw new Error(
+        "Please specify a SUPER_ADMIN_EMAIL environment variable. Exiting ..."
+      );
+    }
+
+    //check password
+    const superAdminPassword = process.env.SUPER_ADMIN_PASSWORD;
+
+    //if no password found exit requesting the variable
+    if (!superAdminPassword) {
+      throw new Error(
+        "Please specify a SUPER_ADMIN_PASSWORD environment variable. Exiting ..."
+      );
+    }
+
+    //hash the password
+    const hashedPassword = await hashPassword(superAdminPassword);
+
+    //create a default admin account and log on screen
+    const newAdmin = new MsAdmin({
+      fullname: "Default system admin account",
+      password: hashedPassword,
+      email: superAdminEmail, //TODO: we need a custom email validator on schema
+      sysdefined: true,
+      role: "superadmin",
+    });
+
+    await newAdmin.save();
+    console.log("\n \t Minimal account created successfully");
+    return newAdmin.id;
+  } catch (error) {
+    console.log(error.message);
+    throw new Error(`An error occured creating account ${error.message}`);
+  }
+};
