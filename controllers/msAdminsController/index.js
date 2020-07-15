9c7024ab7417fa0fdc27f0eefeec8743ef40ca37
Dave <callremz@gmail.com>
2020-07-15 08:09:56 +0100

    feat: add routes to controller

--- controllers/msAdminsController/index.js
+++ controllers/msAdminsController/index.js
@@ -1,8 +1,4 @@
 //POST CONTROLLERS
-const createSingleMsAdmin = require("./createSingleMsAdmin");
-const loginSysAdmin = require("./loginSysAdmin");
-
-module.exports = {
-  createSingleMsAdmin,
-  loginSysAdmin,
-};
+exports.createSingleMsAdmin = require("./createSingleMsAdmin");
+exports.loginSysAdmin = require("./loginSysAdmin");
+exports.changeMsAdminPassword = require("./changeMsAdminPassword");
