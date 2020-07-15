c409ffd8ead5ca3279f5b32da2c4131461071de1
Dave <callremz@gmail.com>
2020-07-15 05:39:24 +0100

    feat: attach route to controller

--- controllers/msAdminsController/index.js
+++ controllers/msAdminsController/index.js
@@ -1,6 +1,8 @@
 //POST CONTROLLERS
 const createSingleMsAdmin = require("./createSingleMsAdmin");
+const loginSysAdmin = require("./loginSysAdmin");
 
 module.exports = {
   createSingleMsAdmin,
+  loginSysAdmin,
 };
--- routes/msadmins.js
+++ routes/msadmins.js
@@ -2,10 +2,18 @@ const express = require("express");
 const { sysAuthMW } = require("../middleware/auth");
 const msAdminsCtrl = require("../controllers/msAdminsController");
 const validMW = require("../middleware/validation");
-const validationRules = require("../utils/validationRules");
+const validationRules = require("../utils/validationRules").msAdmins;
 
 const router = express.Router();
 
+// ------- Unguarded routes ----------
+router.post(
+  "/login",
+  validMW(validationRules.loginAdminSchema),
+  msAdminsCtrl.loginSysAdmin
+);
+
+// --------- DONT TOUCH!!! -------------
 //apply middleware at top of chain
 router.use(sysAuthMW);
 
