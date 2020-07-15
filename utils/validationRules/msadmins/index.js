0f94ed1f362a67ce2d1f47cc647e49a095d68f9b
Dave <callremz@gmail.com>
2020-07-15 17:49:25 +0100

    feat: add validation schema

--- utils/validationRules/msadmins/index.js
+++ utils/validationRules/msadmins/index.js
@@ -2,3 +2,4 @@ exports.createSingleMsAdminSchema = require("./createSingleMsAdminSchema");
 exports.loginAdminSchema = require("./loginAdminSchema");
 exports.changeMsAdminPasswordSchema = require("./changeMsAdminPasswordSchema");
 exports.getAllMsAdminsSchema = require("./getAllMsAdminsSchema");
+exports.getSingleMsAdminSchema = require("./getSingleMsAdminSchema");
