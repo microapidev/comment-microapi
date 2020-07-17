ffa666eadb622d77d1325aa5c57d914b53719b64
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-17 05:41:11 +0100

    lint:fix

--- controllers/msAdminsController/applications/getSingleApplication.js
+++ controllers/msAdminsController/applications/getSingleApplication.js
@@ -1,8 +1,8 @@
-const Applications = require('../../../models/applications');
-const MsAdmin = require('../../../models/msadmins');
-const CommentModel = require('../../../models/comments');
-const CustomError = require('../../../utils/customError');
-const responseHandler = require('../../../utils/responseHandler');
+const Applications = require("../../../models/applications");
+const MsAdmin = require("../../../models/msadmins");
+// const CommentModel = require("../../../models/comments");
+const CustomError = require("../../../utils/customError");
+const responseHandler = require("../../../utils/responseHandler");
 
 /**
  * @author Ekeyekwu Oscar
@@ -24,17 +24,17 @@ const getSingleApplication = async (req, res, next) => {
     //check if msAdmin exists
     const msAdmin = await MsAdmin.findById(msAdminId);
     if (!msAdmin) {
-      next(new CustomError(404, 'MsAdmin account not found'));
+      next(new CustomError(404, "MsAdmin account not found"));
       return;
     }
 
     //get  applications
     const singleApplication = await Applications.findById(
       applicationId
-    ).populate('organizationId');
+    ).populate("organizationId");
 
     if (!singleApplication) {
-      next(new CustomError(404, 'Application not Found'));
+      next(new CustomError(404, "Application not Found"));
       return;
     }
 
@@ -56,19 +56,18 @@ const getSingleApplication = async (req, res, next) => {
       applicationName: singleApplication.name,
       organizationId: singleApplication.organizationId,
       organizationName: singleApplication.organizationId.name,
-    //   totalNumOfComments: commentsCount,
-    //   totalNumOfReplies: repliesCount,
+      //   totalNumOfComments: commentsCount,
+      //   totalNumOfReplies: repliesCount,
     };
   } catch (error) {
-    console.log(error.message);
-    next(new CustomError(400, 'An error occured retrieving Application'));
+    next(new CustomError(400, "An error occured retrieving Application"));
     return;
   }
   return responseHandler(
     res,
     200,
     application,
-    'Application retrieved successfully'
+    "Application retrieved successfully"
   );
 };
 
