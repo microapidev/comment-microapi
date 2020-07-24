50287feb72cc7e0ee190432c025171b249cc1a1d
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-24 07:37:34 +0100

    added period enum in plans model

--- models/plans.js
+++ models/plans.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const Schema = mongoose.Schema;
 
+const PeriodEnum = Object.freeze({ monthly: 1, quarterly: 3, yearly: 12 });
+
 const PlanSchema = new Schema({
   name: {
     type: String,
@@ -18,6 +20,11 @@ const PlanSchema = new Schema({
   },
   maxRequestPerDay: {
     type: Number,
+  },
+  period: {
+    type: Number,
+    enum: PeriodEnum,
+    default: PeriodEnum.monthly,
   },
 });
 PlanSchema.plugin(mongoosePaginate);
