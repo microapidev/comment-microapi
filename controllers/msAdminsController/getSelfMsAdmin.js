a702a4698d0d68d1fce4c0835f5d5a255b0d485c
piouson <piouson@users.noreply.github.com>
2020-07-25 09:23:45 +0100

    Update getSelfMsAdmin.js

--- controllers/msAdminsController/getSelfMsAdmin.js
+++ controllers/msAdminsController/getSelfMsAdmin.js
@@ -22,7 +22,7 @@ const getSelfMsAdmin = async (req, res, next) => {
       return;
     }
     const data = {
-      msAdminId: msAdmin.id,
+      msAdminId: msAdmin._id,
       fullname: msAdmin.fullname,
       email: msAdmin.email,
       role: msAdmin.role,
