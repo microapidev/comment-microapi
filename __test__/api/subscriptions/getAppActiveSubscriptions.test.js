c7aae309083a1e274cb4d58a8661aacaa3a96fac
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-07-26 09:27:24 +0100

    fix: failing test

--- __test__/api/subscriptions/getAppActiveSubscriptions.test.js
+++
@@ -1,7 +0,0 @@
-const getActiveSubscriptions = require("../../../utils/getActiveSubscription");
-
-test("should log active subs", async () => {
-  const getSubs = await getActiveSubscriptions(global.application._id);
-
-  console.log(getSubs);
-});
